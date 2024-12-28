658263c4e3ee007b15f50f3643ac9ecb51cbd417
Darius <darius.clark@proton.me>
2024-12-28 09:54:32 -0500

    chore: move ExecutorSwitch to global, renaming to GlobalExecutor

---
+++ src/global.rs
@@ -0,0 +1,23 @@
+use std::future::Future;
+use crate::{rt, Executor, JoinHandle};
+
+/// Executor that switch between [`TokioExecutor`](rt::tokio::TokioExecutor) and [`WasmExecutor`](rt::wasm::WasmExecutor) at compile time.
+/// If the target is non-wasm32, [`tokio`] would be used, if it is supported.
+/// If the target is wasm32, [`wasm-bindgen-futures`] would be used.
+#[derive(Clone, Copy, Debug, PartialOrd, PartialEq, Eq)]
+pub struct GlobalExecutor;
+
+impl Executor for GlobalExecutor {
+    fn spawn<F>(&self, future: F) -> JoinHandle<F::Output>
+    where
+        F: Future + Send + 'static,
+        F::Output: Send + 'static,
+    {
+        #[cfg(all(feature = "tokio", not(target_arch = "wasm32")))]
+        let executor = rt::tokio::TokioExecutor;
+        #[cfg(target_arch = "wasm32")]
+        let executor = rt::wasm::WasmExecutor;
+
+        executor.spawn(future)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod rt;
+pub mod global;
 
 use std::fmt::{Debug, Formatter};
 
@@ -304,27 +305,6 @@ pub trait Executor {
             _task_handle,
             _channel_tx: tx,
         }
-    }
-}
-
-/// Executor that switch between [`TokioExecutor`](rt::tokio::TokioExecutor) and [`WasmExecutor`](rt::wasm::WasmExecutor) at compile time.
-/// If the target is non-wasm32, tokio would be used, if it is supported.
-/// If the target is wasm32, wasm-bindgen-futures would be used.
-#[derive(Clone, Copy, Debug, PartialOrd, PartialEq, Eq)]
-pub struct ExecutorSwitch;
-
-impl Executor for ExecutorSwitch {
-    fn spawn<F>(&self, future: F) -> JoinHandle<F::Output>
-    where
-        F: Future + Send + 'static,
-        F::Output: Send + 'static,
-    {
-        #[cfg(all(feature = "tokio", not(target_arch = "wasm32")))]
-        let executor = rt::tokio::TokioExecutor;
-        #[cfg(target_arch = "wasm32")]
-        let executor = rt::wasm::WasmExecutor;
-
-        executor.spawn(future)
     }
 }
 

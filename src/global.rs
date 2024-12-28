c6180f3e1b2b6d020513abc05439f09302f9040c
Darius <darius.clark@proton.me>
2024-12-28 14:03:53 -0500

    chore: Update comments and add threadpool executor to GlobalExecutor

--- src/global.rs
+++ src/global.rs
@@ -1,9 +1,10 @@
 use crate::{rt, Executor, JoinHandle};
 use std::future::Future;
 
-/// Executor that switch between [`TokioExecutor`](rt::tokio::TokioExecutor) and [`WasmExecutor`](rt::wasm::WasmExecutor) at compile time.
-/// If the target is non-wasm32, [`tokio`] would be used, if it is supported.
-/// If the target is wasm32, [`wasm-bindgen-futures`] would be used.
+/// Executor that switch between [`TokioExecutor`](rt::tokio::TokioExecutor), [`ThreadpoolExecutor`](rt::threadpool::ThreadpoolExecutor) and [`WasmExecutor`](rt::wasm::WasmExecutor) at compile time.
+/// * If the target is non-wasm32 and "tokio" feature is enabled, [`tokio`] would be used.
+/// * if the target is non-wasm32 and "threadpool" feature is enabled with [`tokio`] feature disabled, [`futures`] [`ThreadPool`](futures::executor::ThreadPool) will be used.
+/// * If the target is wasm32, [`wasm-bindgen-futures`] would be used.
 #[derive(Clone, Copy, Debug, PartialOrd, PartialEq, Eq)]
 pub struct GlobalExecutor;
 
@@ -15,6 +16,8 @@ impl Executor for GlobalExecutor {
     {
         #[cfg(all(feature = "tokio", not(target_arch = "wasm32")))]
         let executor = rt::tokio::TokioExecutor;
+        #[cfg(all(feature = "threadpool", not(feature = "tokio"), not(target_arch = "wasm32")))]
+        let executor = rt::threadpool::ThreadpoolExecutor;
         #[cfg(target_arch = "wasm32")]
         let executor = rt::wasm::WasmExecutor;
 

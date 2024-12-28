e07f6e088963957c4b5afb0dd7082c36b022d3f2
Darius <darius.clark@proton.me>
2024-12-28 11:03:19 -0500

    chore: move ThreadPool into LazyLock

--- src/rt/threadpool.rs
+++ src/rt/threadpool.rs
@@ -1,24 +1,20 @@
 use std::fmt::{Debug, Formatter};
 use std::future::Future;
+use std::sync::{LazyLock};
+use futures::executor::ThreadPool;
 use futures::future::{AbortHandle, Abortable};
 use crate::{Executor, InnerJoinHandle, JoinHandle};
 
-#[derive(Clone)]
-pub struct ThreadPoolExecutor {
-    pool: futures::executor::ThreadPool,
-}
+const THREADPOOL_EXECUTOR: LazyLock<ThreadPool> = LazyLock::new(|| {
+    ThreadPool::new().unwrap()
+});
+
+#[derive(Clone, Copy, Default)]
+pub struct ThreadPoolExecutor;
 
 impl Debug for ThreadPoolExecutor {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("ThreadPoolExecutor").finish()
-    }
-}
-
-impl Default for ThreadPoolExecutor {
-    fn default() -> Self {
-        Self {
-            pool: futures::executor::ThreadPool::new().unwrap(),
-        }
     }
 }
 
@@ -36,12 +32,67 @@ impl Executor for ThreadPoolExecutor {
             let _ = tx.send(val);
         };
 
-        self.pool.spawn_ok(fut);
+        THREADPOOL_EXECUTOR.spawn_ok(fut);
         let inner = InnerJoinHandle::CustomHandle {
             inner: Some(rx),
             handle: abort_handle,
         };
 
         JoinHandle { inner }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use futures::channel::mpsc::Receiver;
+    use crate::Executor;
+    use super::ThreadPoolExecutor;
+
+    async fn task(tx: futures::channel::oneshot::Sender<()>) {
+        futures_timer::Delay::new(std::time::Duration::from_secs(5)).await;
+        let _ = tx.send(());
+        unreachable!();
+    }
+
+    #[test]
+    fn default_abortable_task() {
+        let executor = ThreadPoolExecutor::default();
+
+        let (tx, rx) = futures::channel::oneshot::channel::<()>();
+        let handle = executor.spawn_abortable(task(tx));
+        drop(handle);
+
+        futures::executor::block_on(async move {
+            let result = rx.await;
+            assert!(result.is_err());
+        });
+    }
+
+    #[test]
+    fn task_coroutine() {
+        use futures::stream::StreamExt;
+        let executor = ThreadPoolExecutor::default();
+
+        enum Message {
+            Send(String, futures::channel::oneshot::Sender<String>),
+        }
+
+        let mut task = executor.spawn_coroutine(|mut rx: Receiver<Message>| async move {
+            while let Some(msg) = rx.next().await {
+                match msg {
+                    Message::Send(msg, sender) => {
+                        sender.send(msg).unwrap();
+                    }
+                }
+            }
+        });
+
+        let (tx, rx) = futures::channel::oneshot::channel::<String>();
+        let msg = Message::Send("Hello".into(), tx);
+        futures::executor::block_on(async move {
+            task.send(msg).await.unwrap();
+            let resp = rx.await.unwrap();
+            assert_eq!(resp, "Hello");
+        });
     }
 }
